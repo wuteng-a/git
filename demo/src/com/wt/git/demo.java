45f56cb858aaa4065ca857e11d6cf26b264451a5
wuteng_a <3088004774@qq.com>
2019-10-17 11:46:25 +0800

    Initial commit

---
+++ demo/src/com/wt/git/demo.java
@@ -0,0 +1,12 @@
+package com.wt.git;
+
+/**
+ * @author吴腾
+ * @company
+ * @create  2019-10-17 11:42
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
